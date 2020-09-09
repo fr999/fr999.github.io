14550657bfb1c1330074d8a39a85057c2a110a8f
LordVenom <a_furbyz@hotmail.com>
2020-09-10 00:26:35 +0200

    test

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-const APIURL =
-    "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+//const APIURL =  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+
+const APIURL = "https://raw.githubusercontent.com/LordVenom/f999/master/_data/com.json";
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCHAPI =
     "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
@@ -25,14 +26,14 @@ function showMovies(movies) {
     main.innerHTML = "";
 
     movies.forEach((movie) => {
-        const { poster_path, title, vote_average, overview } = movie;
+        const { backdrop, title, vote_average, overview } = movie;
 
         const movieEl = document.createElement("div");
         movieEl.classList.add("movie");
 
         movieEl.innerHTML = `
             <img
-                src="${IMGPATH + poster_path}"
+                src="${backdrop}"
                 alt="${title}"
             />
             <div class="movie-info">
@@ -61,14 +62,3 @@ function getClassByRate(vote) {
     }
 }
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    const searchTerm = search.value;
-
-    if (searchTerm) {
-        getMovies(SEARCHAPI + searchTerm);
-
-        search.value = "";
-    }
-});
