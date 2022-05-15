0d23937dd99e546c6fc7a74241e1ed99813c4e3c
Lord Venom <a_furbyz@hotmail.com>
2022-05-15 12:49:40 +0200

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,13 @@
 document.write(`<div class="main-menu">
-<div class="genre">Tri</div>
-<div class="book-types">
- <a href="javascript:void(0)" id="genre_all" class="book-type active">Ajout</a>
+<div class="genre">Navigation</div>
+<div class="book-types" id="previous">
+</div>
+</div>`);
+
+
+/* <a href="javascript:void(0)" id="genre_all" class="book-type active">Ajout</a>
  <a href="javascript:void(0)" id="genre_title" class="book-type"> Titre</a>
  <a href="javascript:void(0)" id="genre_release_date" class="book-type"> Date</a>
  <a href="javascript:void(0)" id="genre_vote_count" class="book-type"> Vote</a>
  <a href="javascript:void(0)" id="genre_version" class="book-type"> Version</a>
- <a href="javascript:void(0)" id="genre_creator" class="book-type"> Createur</a>
-</div>
-</div>`);
+ <a href="javascript:void(0)" id="genre_creator" class="book-type"> Createur</a>*/
