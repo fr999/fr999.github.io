eb1a6fe9bb2d50f38e74b4cbde62248b4d5e1452
Lord Venom <a_furbyz@hotmail.com>
2022-06-06 13:43:41 +0200

    Update header.js

--- js/header.js
+++ js/header.js
@@ -5,37 +5,39 @@ document.write(`<div class="book-store">
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
     <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
   <a href="./index.html">Accueil</a>
- </div>
+ </div><div class="border-right"></div>
   <div class="browse-category">
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
       <circle cx="12" cy="12" r="10"></circle><path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"></path><line x1="12" y1="17" x2="12.01" y2="17"></line></svg>
       <a href="./help.html">Aide</a>
+ </div><div class="border-right"></div>
+   <div class="browse-category">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
+     <div class="search-bar">
+      <form id="search_form">
+        <input id="search" type="text" placeholder="Recherche" /></input>
+      </form>
+    </div>
  </div>
- <div class="search-bar">
-   <form id="search_form">
-  <input id="search" type="text" placeholder="Recherche" />
-</form>
- </div>
+
 </div>
-<div class="header-title">Renpy Traduction Français<span> F999 </span></div>
+<div class="header-title">Renpy Traduction Française F999</div>
 <div class="profile">
 <div id="color" class="profile-color">
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu">
 <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"/></svg>
  <path d="M3 12h18M3 6h18M3 18h18" /></svg>
  <a href="#">Couleur</a>
-</div>
+</div><div class="border-right"></div>
  <div class="profile-menu">
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu">
    <path d="M3 12h18M3 6h18M3 18h18" /></svg>
-   <a href="#">Genre</a>
+   <a href="#" id="menugenre">Genre</a>
  </div>
   <nav role="navigation">
     <div id="menuToggle">
-      <input name="toggle" type="checkbox" />
-      <label for="toggle"></label>
       <ul id="menu">
-        <a href="#"><li>Vide</li></a>
+        <a href=""><li>Vide</li></a>
       </ul>
     </div>
   </nav></div></div>
