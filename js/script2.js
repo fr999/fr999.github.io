40e33136e58116e5e2db8ce431125746a37ae8f6
Lord Venom <a_furbyz@hotmail.com>
2022-06-19 16:11:15 +0200

    Update script2.js

--- js/script2.js
+++ js/script2.js
@@ -87,7 +87,7 @@ function showMovies(movies) {
         
         if (genre.length > 5) {
           
-          repoNAME  = repoNAME + '<div class="like-name" title="'+genre+'">plus...</div>';
+          repoNAME  = repoNAME + '<div class="like-name" data-genre="'+genre+'">plus...</div>';
         }
         
 
@@ -97,7 +97,7 @@ function showMovies(movies) {
         
         if (version_translate && version != version_translate) {
           
-          notversion =  '<div class="like-profile yellow" title="Version du jeux diffèrente de la traduction"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z"></path><line x1="12" y1="9" x2="12" y2="13"></line><line x1="12" y1="17" x2="12.01" y2="17"></line></svg></div>'
+          notversion =  '<div class="like-profile yellow" data-title="Version du jeux diffèrente de la traduction"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z"></path><line x1="12" y1="9" x2="12" y2="13"></line><line x1="12" y1="17" x2="12.01" y2="17"></line></svg></div>'
           
         }
 
