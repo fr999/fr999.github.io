d8abe9a623b9a8f750f7ad153d69aef19732ab9b
Lord Venom <a_furbyz@hotmail.com>
2022-06-22 09:47:38 +0200

    Update header.js

--- js/header.js
+++ js/header.js
@@ -6,16 +6,11 @@ document.write(`<div class="book-store">
     <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
   <a href="./index.html">Accueil</a>
  </div><div class="border-right"></div>
-  <div class="browse-category">
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-      <circle cx="12" cy="12" r="10"></circle><path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"></path><line x1="12" y1="17" x2="12.01" y2="17"></line></svg>
-      <a href="./help.html">Aide</a>
- </div><div class="border-right"></div>
    <div class="browse-category">
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
      <div class="search-bar">
       <form id="search_form">
-        <input id="search" type="text" placeholder="Recherche" /></input>
+        <input id="search" type="text" placeholder="Recherche"  size="10"/></input>
       </form>
     </div>
  </div>
@@ -43,3 +38,10 @@ document.write(`<div class="book-store">
   </nav></div></div>
 <div id="slider-header">
 <div id="slider" class="slider"></div></div>`);
+
+
+  /*<div class="browse-category">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <circle cx="12" cy="12" r="10"></circle><path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"></path><line x1="12" y1="17" x2="12.01" y2="17"></line></svg>
+      <a href="./help.html">Aide</a>
+ </div><div class="border-right"></div>*/
